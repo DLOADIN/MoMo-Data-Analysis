6f770c869972462e43e5bda9e50ae045dd0b4329
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-17 18:46:19 +0200

    adding routes

--- MOMO_DASHBOARD/momo/views.py
+++ MOMO_DASHBOARD/momo/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
+from .models import (
+    AirtimePayments,
+    MoneyRecieved,
+    TransfersToMomo,
+    BankDeposits,
+    PaymentToCode,
+    CashPowerPayments,
+    Withdrawals,
+    PaymentsToThirdParties,
+    BundlesAndPacks,
+)
+
 
 # Create your views here.
+def home(request):
+    return render(request, "momo/index.html")
+
+
+def airtime(request):
+    return render(
+        request, "momo/airtime.html", {"airtime": AirtimePayments.objects.all()}
+    )
+
+
+def transfers_to_momo(request):
+    return render(
+        request, "momo/momotransfer.html", {"momotransfers": TransfersToMomo.objects.all()}
+    )
+
+
+def bankdeposits(request):
+    return render(request, "momo/bankdepo.html", {"bankdepo": BankDeposits.objects.all()})
+
+
+def payment_to_code(request):
+    return render(
+        request, "momo/paymenttocode.html", {"paymenttocode": PaymentToCode.objects.all()}
+    )
+
+
+def cashpower_payments(request):
+    return render(
+        request, "momo/cashpower.html", {"cashpower": CashPowerPayments.objects.all()}
+    )
+
+
+def payments_to_third_parties(request):
+    return render(
+        request, "momo/transactions.html", {"transactions": PaymentsToThirdParties.objects.all()}
+    )
+
+
+def withdrawals(request):
+    return render(request, "momo/withdrawals.html", {"withdrawals": Withdrawals.objects.all()})
+
+
+def bundles_and_packs(request):
+    return render(
+        request, "momo/data.html", {"data": BundlesAndPacks.objects.all()}
+    )
+
+
+def moneyreceived(request):
+    return render(
+        request, "momo/incomingmoney.html", {"moneyrecieved": MoneyRecieved.objects.all()}
+    )
