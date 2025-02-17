c659eb5a3793bf4f8f24dea8aadb1a3767b4d8fe
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-02-17 18:58:32 +0200

    establishing routes

--- MOMO_DASHBOARD/momo/urls.py
+++ MOMO_DASHBOARD/momo/urls.py
@@ -1,4 +1,17 @@
 from django.urls import path
 
-app_name  = "momo"
-urlpatterns = []
+from . import views
+
+app_name = "momo"
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("airtime", views.airtime, name="airtime"),
+    path("momotransfer", views.transfers_to_momo, name="transferstomomo"),
+    path("paymenttocode", views.payment_to_code, name="paymenttocode"),
+    path("cashpower", views.cashpower_payments, name="cashpower"),
+    path("transactions", views.payments_to_third_parties, name="transactions"),
+    path("withdrawals", views.withdrawals, name="withdrawals"),
+    path("data", views.bundles_and_packs, name="data"),
+    path("incomingmoney", views.moneyreceived, name="moneyrecieved"),
+    path("bankdepo", views.bankdeposits, name="bankdepo"),
+]
